943456e570d440e38041f964e9c071557beecb69
Ryan Rafilianto <ryan.rafilianto@sicepat.com>
2022-01-29 12:28:46 +0700

    change header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -9,8 +9,10 @@ const Header = () => {
         <div className="text-center mb-28">
           <p className="text-3xl font-primary">MEDINA AULIA</p>
           <p className="text-3xl font-primary mb-16">RYAN RAFILIANTO</p>
-          <p>Sunday</p>
-          <p>February 06, 2022</p>
+          <div className="border-b border-t py-5 mx-10 font-primary">
+            <p>Sunday</p>
+            <p>February 06, 2022</p>
+          </div>
         </div>
       </div>
     </div>
